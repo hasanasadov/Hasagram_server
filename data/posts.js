450899370875b5f1fd35e775d7fcb848304ce5c1
hasanasadov <hasanaliasadov@gmail.com>
2024-10-12 13:16:23 +0400

    some tests

--- data/posts.js
+++ data/posts.js
@@ -1,4 +1,8 @@
 // In-memory array to store posts
-const posts = [];
+const posts = [{
+    id: 1,
+    title: 'Hello World',
+    content: 'This is my first post on this blog.',
+}];
 
 module.exports = posts;
